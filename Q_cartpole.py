e3993a09ef09ec02011c62384db266e9798ffd54
Satish <satishmsid@gmail.com>
2020-04-30 05:48:14 -0400

    Added Sample Code and Initial Scripts

---
+++ Q_cartpole.py
@@ -0,0 +1,11 @@
+# import gym
+# import numpy as np
+
+# class Q_cartpole():
+#     def __init__(self,Environment):
+#         self.Q_table = np.zeros([Environment.action_space.shape[0],Environment.observation_space.shape[0]])
+#         print(self.Q_table.shape)
+
+# env = gym.make('CartPole-v0')
+# # Q = Q_cartpole(env)
+# print(np.array(env.action_space))
