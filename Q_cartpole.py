1cfeef850c16e211ddcb1dd1a8bc48ed0ab98645
Satish <satishmsid@gmail.com>
2020-05-18 22:29:52 -0400

    Added Logging

--- Q_cartpole.py
+++ Q_cartpole.py
@@ -1,9 +1,14 @@
 import gym
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 class My_QL():
-    def __init__(self,Environment,alpha = 1.0,revolving_decay = True, min_gamma = 0.1,min_epsilon = 0.1,MAX_STEPS = 300,MAX_EPISODES = 100,MAX_STATES = 10,buckets=(1, 1, 6, 12,),monitor=False,SHOW_EVERY=1000):
+    # For stats
+    ep_rewards = []
+    aggr_ep_rewards = {'ep': [], 'avg': [], 'max': [], 'min': []}
+
+    def __init__(self,Environment,alpha = 1.0,revolving_decay = True, min_gamma = 0.1,min_epsilon = 0.1,MAX_STEPS = 300,MAX_EPISODES = 100,MAX_STATES = 10,buckets=(1, 1, 6, 12,),monitor=False,SHOW_EVERY=1000,STATS_EVERY = 10):
         self.env = Environment
         # self.gamma = gamma # Q learning rate
         self.min_gamma = min_gamma # learning rate
@@ -22,12 +27,14 @@ class My_QL():
         self.ada_divisor = 25 # only for development purposes
         self.SHOW_EVERY = SHOW_EVERY #Render every 100 episodes
         self.revolving_decay = revolving_decay
+        self.STATS_EVERY = STATS_EVERY
         if MAX_STEPS is not None: 
             self.env._max_episode_steps = MAX_STEPS
         if monitor: 
             self.env = gym.wrappers.Monitor(self.env, 'tmp/cartpole-1', force=True) # record results for upload
 
     def run(self):
+        self.episode_reward = 0
         counter = 0
         ep_batch = 0
         for e in range(self.MAX_EPISODES):
@@ -43,45 +50,37 @@ class My_QL():
             
             # Setting epsilon and gamma
             epsilon = self.get_epsilon(ep_batch)
-            # epsilon = 0.5
             gamma = self.get_gamma(ep_batch)
-            # gamma = 0.9
-            # gamma = self.gamma
             
             #discritizing current state
-            curr_state = self.discretize(self.env.reset()) # self.assign_bins(obs)
+            curr_state = self.discretize(self.env.reset())
 
+            # Training
             for i in range(self.MAX_STEPS):
-                # Render Environment
-                if(e % self.SHOW_EVERY == 0):
-                    epsilon = 0
-                    gamma = 0
-                    self.env.render()
-                # getting random probability
-                prob = np.random.rand()
-
-                # implementing epsilon greedy policy
-                if(prob < epsilon):
-                    u = self.env.action_space.sample()
-                else:
-                    u = np.argmax(self.Q[curr_state])
-
-                # Performing the selected action
-                Obs, reward, done, info = self.env.step(u)
-
-                # Discritizing new_state
-                new_state = self.discretize(Obs)
-
-                # Updating Q-Table
-                TD_error = reward + self.alpha * (np.max(self.Q[new_state])-self.Q[curr_state][u])
-                self.Q[curr_state][u] = self.Q[curr_state][u]+ gamma * TD_error
-
-                curr_state = new_state
-
+                done,curr_state = self.step(e,epsilon,gamma,curr_state)
                 if done:
                     break
-            
-            print(f"Episode: {e}, Steps: {i}, Epsilon: {epsilon:.2f}, {curr_state}")
+
+            if(e % self.SHOW_EVERY == 0):
+                #discritizing current state
+                curr_state = self.discretize(self.env.reset())
+                for i in range(self.MAX_STEPS):
+                    done,curr_state = self.step(e,0,0,curr_state)
+                    self.env.render()
+                    if done:
+                        break
+
+            # print(f"Episode: {e}, Steps: {i}, Epsilon: {epsilon:.2f}, {curr_state}")
+
+            # Saving cost funcitons for visualization
+            self.ep_rewards.append(self.episode_reward)
+            if not e % self.STATS_EVERY:
+                self.average_reward = sum(self.ep_rewards[-self.STATS_EVERY:])/self.STATS_EVERY
+                self.aggr_ep_rewards['ep'].append(e)
+                self.aggr_ep_rewards['avg'].append(self.average_reward)
+                self.aggr_ep_rewards['max'].append(max(self.ep_rewards[-self.STATS_EVERY:]))
+                self.aggr_ep_rewards['min'].append(min(self.ep_rewards[-self.STATS_EVERY:]))
+                print(f'Episode: {e:>5d}, average reward: {self.average_reward:>4.1f}, current epsilon: {epsilon:>1.2f}')
 
             if i == self.MAX_STEPS -1:
                 counter +=1
@@ -108,6 +107,35 @@ class My_QL():
 
                 if(done):
                     break
+
+    def step(self,e,epsilon,gamma,curr_state):
+        
+        # getting random probability
+        prob = np.random.rand()
+
+        # implementing epsilon greedy policy
+        if(prob < epsilon):
+            u = self.env.action_space.sample()
+        else:
+            u = np.argmax(self.Q[curr_state])
+
+        # Performing the selected action
+        Obs, reward, done, info = self.env.step(u)
+
+        # Getting reward for one episode
+        self.episode_reward += reward  
+
+        # Discritizing new_state
+        new_state = self.discretize(Obs)
+
+        # Updating Q-Table
+        TD_error = reward + self.alpha * (np.max(self.Q[new_state])-self.Q[curr_state][u])
+        self.Q[curr_state][u] = self.Q[curr_state][u]+ gamma * TD_error
+
+        curr_state = new_state
+
+        return done,new_state
+        
     # --------------------------------------------------------------------------------------------------------------------------------------
 
     def discretize(self,observation):
@@ -122,17 +150,6 @@ class My_QL():
         return tuple(state.tolist())     
     
     # --------------------------------------------------------------------------------------------------------------------------------------
-    #region Discretize
-    # def Discretize(self,obs):
-    #     upper_bounds = [self.env.observation_space.high[0], 0.5, self.env.observation_space.high[2], math.radians(50)]
-    #     lower_bounds = [self.env.observation_space.low[0], -0.5, self.env.observation_space.low[2], -math.radians(50)]
-    #     # obs = np.amin([np.amax([obs,np.array(lower_bounds)],axis=0),np.array(upper_bounds)],axis=0)
-    #     ratios = [(obs[i] + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i]) for i in range(len(obs))]
-    #     new_obs = [int(round((self.buckets[i] - 1) * ratios[i])) for i in range(len(obs))]
-    #     new_obs = [min(self.buckets[i] - 1, max(0, new_obs[i])) for i in range(len(obs))]
-    #     return tuple(new_obs)
-    #endregion
-
     # Choosing action based on epsilon-greedy policy
     def choose_action(self, state, epsilon):
         return self.env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(self.Q[state])
@@ -297,10 +314,15 @@ class Q_Learning():
             
 
 env = gym.make('CartPole-v0')
-Q = My_QL(env,min_epsilon = 0.1,MAX_STEPS = 300,MAX_EPISODES = 100000,MAX_STATES = 5,SHOW_EVERY = 10000,revolving_decay = False, buckets=(10,10,20,20))
+Max_Episodes = 10_000
+Q = My_QL(env,min_epsilon = 0.1,MAX_STEPS = 300,MAX_EPISODES = Max_Episodes,MAX_STATES = 5,SHOW_EVERY = 100,STATS_EVERY = 10,revolving_decay = True, buckets=(1,1,6,12))
 Q.run()
 Q.env.close()
-# print(Q.env.observation_space.high-Q.env.observation_space.low)
-# print(Q.Q.shape,"\n",[Q.discretize(Q.env.reset())])
-# Q = Q_Learning(env,min_epsilon = 0.1,MAX_STEPS = 300,MAX_EPISODES = 1000,MAX_STATES = 10)
-# print(Q.Q.shape,"\n",[Q.discretize(Q.env.reset())])
+
+plt.plot(Q.aggr_ep_rewards['ep'], Q.aggr_ep_rewards['avg'], label="average rewards")
+plt.plot(Q.aggr_ep_rewards['ep'], Q.aggr_ep_rewards['max'], label="max rewards")
+plt.plot(Q.aggr_ep_rewards['ep'], Q.aggr_ep_rewards['min'], label="min rewards")
+plt.legend(loc=4)
+plt.grid(True)
+plt.savefig(f"Reward_vs_Episodes_{Max_Episodes}.png")
+plt.show()
